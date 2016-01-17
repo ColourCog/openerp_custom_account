813dcc57bd2f8b4eecc60940c0b00c1c24d86b19
Stanislas H.B. Sodonon <bidossessi@linuxbenin.com>
2016-01-17 13:30:24 +0100

    LAST LAST check

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name" : "Custom EIS Modifications",
-    "version" : "4.19",
+    "version" : "4.20",
     "category" : "Accounting",
     "sequence": 60,
     "complexity" : "normal",
