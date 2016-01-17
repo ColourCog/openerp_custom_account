7694e352fef9b97e7f2c0c7ce160e71586842f0b
Stanislas H.B. Sodonon <bidossessi@linuxbenin.com>
2016-01-17 13:33:10 +0100

    OK I lied

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name" : "Custom EIS Modifications",
-    "version" : "4.21",
+    "version" : "4.22",
     "category" : "Accounting",
     "sequence": 60,
     "complexity" : "normal",
