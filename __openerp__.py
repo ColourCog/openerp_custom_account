8aac538fd082f965c6e36566716d896627c96b54
Stanislas H.B. Sodonon <bidossessi@linuxbenin.com>
2016-01-17 12:30:42 +0100

    version bump

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name" : "Custom EIS Modifications",
-    "version" : "4.6",
+    "version" : "4.7",
     "category" : "Accounting",
     "sequence": 60,
     "complexity" : "normal",
