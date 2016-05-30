a6badd4f6e8eb732f1ca6acffc00da6c9400f9d7
Bidossessi <bidossessi@colourcog.com>
2016-05-30 11:25:21 +0100

    added uba charges

--- account_invoice.py
+++ account_invoice.py
@@ -34,12 +34,21 @@ class account_invoice(osv.osv):
 
     def _get_cash_journal(self, cr, uid, context=None):
         cur_obj = self.pool.get('account.journal')
-        currency_ids = cur_obj.search(
+        journal_ids = cur_obj.search(
             cr,
             uid,
             [("type", "=", "cash")],
             context=context)
-        return currency_ids[0]
+        return journal_ids[0]
+
+    def _get_uba_journal(self, cr, uid, context=None):
+        cur_obj = self.pool.get('account.journal')
+        journal_ids = cur_obj.search(
+            cr,
+            uid,
+            [("code", "=", "BNK3")],
+            context=context)
+        return journal_ids[0]
 
     def _get_vouchers(self, cr, uid, ids, context=None):
         _logger.debug("In _get_vouchers")
@@ -208,6 +217,99 @@ class account_invoice(osv.osv):
 
         return True
 
+    def invoice_pay_bank_charges(self, cr, uid, ids, context=None):
+        wf_service = netsvc.LocalService('workflow')
+        if context is None:
+            context = {}
+        if not ids:
+            return []
+        real_ids = []
+        for inv in self.browse(cr, uid, ids, context=context):
+            if inv.state in ('draft', 'proforma', 'proforma2'):
+                wf_service.trg_validate(
+                        uid,
+                        'account.invoice',
+                        inv.id,
+                        'invoice_open',
+                        cr)
+            if inv.state != 'paid':
+                real_ids.append(inv.id)
+        return self._create_cash_voucher(cr, uid, real_ids, context=context)
+
+    def _create_uba_voucher(self, cr, uid, ids, context):
+        ctx = context.copy()
+        ctx.update({'account_period_prefer_normal': True})
+
+        voucher_obj = self.pool.get('account.voucher')
+        inv_obj = self.pool.get('account.invoice')
+        journal_obj = self.pool.get('account.journal')
+        move_line_obj = self.pool.get('account.move.line')
+        period_obj = self.pool.get('account.period')
+        company_id = self.pool.get('res.users').browse(
+            cr,
+            uid,
+            uid,
+            context=ctx).company_id.id
+
+        for inv in inv_obj.browse(cr, uid, ids, context=ctx):
+            name = _('Bank charges %s') % (inv.partner_id.name)
+            partner_id = inv.partner_id.id
+            journal = journal_obj.browse(
+                cr,
+                uid,
+                self._get_uba_journal(
+                    cr,
+                    uid,
+                    context=ctx),
+                context=ctx)
+            amt = inv.residual
+            credit_account_id = inv.account_id.id
+            move_line_id = move_line_obj.search(
+                cr,
+                uid,
+                [('move_id', '=', inv.move_id.id),
+                    ('invoice', '=', inv.id)],
+                context=ctx)[0]
+            voucher = {
+                'journal_id': journal.id,
+                'company_id': company_id,
+                'partner_id': partner_id,
+                'type': 'payment',
+                'name': name,
+                'reference': inv.reference,
+                'account_id': journal.default_credit_account_id.id,
+                'amount': amt > 0.0 and amt or 0.0,
+                'date': inv.date_invoice,
+                'date_due': inv.date_invoice,
+                'period_id': period_obj.find(
+                    cr,
+                    uid,
+                    inv.date_invoice,
+                    context=ctx)[0],
+                }
+
+            vl = (0, 0, {
+                'name': inv.name,
+                'move_line_id': move_line_id,
+                'reconcile': True,
+                'amount': amt > 0.0 and amt or 0.0,
+                'account_id': credit_account_id,
+                'type': amt > 0.0 and 'dr' or 'cr',
+                })
+            voucher['line_ids'] = [vl]
+            voucher_id = voucher_obj.create(
+                cr,
+                uid,
+                voucher,
+                context=ctx)
+            voucher_obj.button_proforma_voucher(
+                cr,
+                uid,
+                [voucher_id],
+                context)
+            inv_obj.confirm_paid(cr, uid, [inv.id], context=ctx)
+
+        return True
     _columns = {
         'extra_currency_id': fields.many2one(
                                 'res.currency',
